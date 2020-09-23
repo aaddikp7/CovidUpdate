7d0a087967c3878aaf0ea8d9962a466897e1cd9b
aaditya dahiya <aadikp149@Gmail.com>
2020-09-23 21:56:18 +0530

    first commit

---
+++ Covid/CovidUpdate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CovidUpdate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
